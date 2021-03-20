f016b81971054494cf27a15e09bfdbeba0236aad
Simona Cotin <simona.cotin@gmail.com>
2021-03-20 15:42:04 +0100

    don't reuse connection

--- products-get/index.js
+++ products-get/index.js
@@ -4,23 +4,17 @@ const auth = {
   user: process.env.user,
   password: process.env.password,
 };
-let db = null;
 const loadDB = async () => {
-  if (db) {
-    return db;
-  }
   const client = await MongoClient.connect(
     `mongodb://${process.env.url}:${process.env.port}/?ssl=true`,
     {
       auth: auth,
     }
   );
-  db = client.db("serverless");
-  return db;
+  return client.db("serverless");
 };
 module.exports = async function (context, req) {
   try {
-    //const products = data.getProducts();
     const database = await loadDB();
     let products = await database.collection("Product").find().toArray();
 
